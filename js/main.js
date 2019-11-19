9a0632584a14a740f7da81e10b1f65c859b5d5fe
Joy Xie <a556987@gmail.com>
2019-11-19 13:27:46 +0800

    change images

--- js/main.js
+++ js/main.js
@@ -1,5 +1,30 @@
 // HTML 讀取後執行
 $(function(){
+    /* 預先載入圖片 */
+    function preloadImg(image) {
+        let img = new Image();
+        img.src = image;
+    }
+
+    let pics = [
+        'img/test_1.jpg',
+        'img/test_2.jpg',
+        'img/test_3.jpg',
+        'img/test_4.jpg',
+        'img/test_5.jpg',
+        'img/test_6.jpg',
+        'img/test_7.jpg',
+        'img/test_8.jpg',
+        'img/test_9.jpg',
+        'img/test_10.jpg',
+        'img/cover.jpg',
+        'img/bg.jpg'
+    ];
+
+    for(let i=0;i<pics.length;i++){
+        preloadImg(pics[i]);
+    }
+
     //--------------------
     // 變數宣告
     //--------------------
